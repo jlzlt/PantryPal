3aaab8a6b5ece3926e2ce5db3080e6b958250e11
jlzlt <juliusrackys@gmail.com>
2025-07-08 09:56:34 +0300

    Debug setting from env.

--- pantrypal/settings.py
+++ pantrypal/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
